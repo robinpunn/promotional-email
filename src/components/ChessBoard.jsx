2d9d355f53cdffa4d8be4710050afc33d13d2e1e
Robin Punnoose <robin.punnoose@protonmail.com>
2023-03-04 02:13:10 -0600

    created chessboard with nested loops

--- src/components/ChessBoard.jsx
+++ src/components/ChessBoard.jsx
@@ -1,121 +1,44 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import "./ChessBoard.css";
 
-function ChessBoard() {
+function ChessBoard({ setBoard }) {
+  const boardRef = useRef(null);
+
+  useEffect(() => {
+    setBoard(boardRef.current);
+  }, [setBoard]);
+
   return (
-    <table className="board">
+    <table ref={boardRef}>
       <tbody>
-        <tr className="rank-8">
-          <td id="a8" className="white">
-            <p className="number">8</p>
-          </td>
-          <td className="green"></td>
-          <td className="white"></td>
-          <td className="green"></td>
-          <td className="white"></td>
-          <td className="green"></td>
-          <td className="white"></td>
-          <td className="green"></td>
-        </tr>
-        <tr className="rank-7">
-          <td id="seven" className="green">
-            <p className="number">7</p>
-          </td>
-          <td className="white"></td>
-          <td className="green"></td>
-          <td className="white"></td>
-          <td className="green"></td>
-          <td className="white"></td>
-          <td className="green"></td>
-          <td className="white"></td>
-        </tr>
-        <tr className="rank-6">
-          <td id="six" className="white">
-            <p className="number">6</p>
-          </td>
-          <td className="green"></td>
-          <td className="white"></td>
-          <td className="green"></td>
-          <td className="white"></td>
-          <td className="green"></td>
-          <td className="white"></td>
-          <td className="green"></td>
-        </tr>
-        <tr className="rank-5">
-          <td id="five" className="green">
-            <p className="number">5</p>
-          </td>
-          <td className="white"></td>
-          <td className="green"></td>
-          <td className="white"></td>
-          <td className="green"></td>
-          <td className="white"></td>
-          <td className="green"></td>
-          <td className="white"></td>
-        </tr>
-        <tr className="rank-4">
-          <td id="four" className="white">
-            <p className="number">4</p>
-          </td>
-          <td className="green"></td>
-          <td className="white"></td>
-          <td className="green"></td>
-          <td className="white"></td>
-          <td className="green"></td>
-          <td className="white"></td>
-          <td className="green"></td>
-        </tr>
-        <tr className="rank-3">
-          <td id="three" className="green">
-            <p className="number">3</p>
-          </td>
-          <td className="white"></td>
-          <td className="green"></td>
-          <td className="white"></td>
-          <td className="green"></td>
-          <td className="white"></td>
-          <td className="green"></td>
-          <td className="white"></td>
-        </tr>
-        <tr className="rank-2">
-          <td id="two" className="white">
-            <p className="number">2</p>
-          </td>
-          <td className="green"></td>
-          <td className="white"></td>
-          <td className="green"></td>
-          <td className="white"></td>
-          <td className="green"></td>
-          <td className="white"></td>
-          <td className="green"></td>
-        </tr>
-        <tr className="rank-1">
-          <td id="one" className="green">
-            <p className="number">1</p>
-            <p className="letter">a</p>
-          </td>
-          <td className="white">
-            <p className="letter">b</p>
-          </td>
-          <td className="green">
-            <p className="letter">c</p>
-          </td>
-          <td className="white">
-            <p className="letter">d</p>
-          </td>
-          <td className="green">
-            <p className="letter">e</p>
-          </td>
-          <td className="white">
-            <p className="letter">f</p>
-          </td>
-          <td className="green">
-            <p className="letter">g</p>
-          </td>
-          <td className="white">
-            <p className="letter">h</p>
-          </td>
-        </tr>
+        {[...Array(8)].map((_, i) => (
+          <tr key={i}>
+            {[...Array(8)].map((_, j) => {
+              const id = `${String.fromCharCode(97 + j)}${8 - i}`;
+              const className = (i + j) % 2 === 0 ? "white" : "green";
+              const letter = String.fromCharCode(97 + j);
+              const number = 8 - i;
+              const isLetterCell = i === 7 && j !== 0;
+              const isNumberCell = j === 0 && i !== 7;
+              return isLetterCell ? (
+                <td key={id} id={id} className={className}>
+                  <p className="letter">{letter}</p>
+                </td>
+              ) : isNumberCell ? (
+                <td key={id} id={id} className={className}>
+                  <p className="number">{number}</p>
+                </td>
+              ) : i === 7 && j === 0 ? (
+                <td key={id} id={id} className={className}>
+                  <p className="letter">{letter}</p>
+                  <p className="number">{number}</p>
+                </td>
+              ) : (
+                <td key={id} id={id} className={className}></td>
+              );
+            })}
+          </tr>
+        ))}
       </tbody>
     </table>
   );
